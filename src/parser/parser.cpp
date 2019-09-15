e8111a2cd159a0228e5d5f3d2a5dd1f1e9f6a916
Reed Taylor <reed_git@reedtaylor.com>
2019-09-15 00:41:42 -0400

    logging cleanup

--- src/parser/parser.cpp
+++ src/parser/parser.cpp
@@ -15,9 +15,8 @@ bool Parser::IsWellFormed(const std::string message) {
     U16 command = matches[1].str()[0];
     U8 target_length_bytes = getLengthForCID(command);
     U8 actual_length_hexchars = matches[2].str().length();
-    LOG(INFO) << "Hex string: " << matches[2].str();
     if (actual_length_hexchars != target_length_bytes * 2) {
-      LOG(WARNING) << "command = " << command << " got " << actual_length_hexchars / 2 << " bytes in hex, expected " << (int)target_length_bytes << " bytes";
+      LOG(WARNING) << "command = " << command << " got " << actual_length_hexchars << " characters in hex, expected " << (int)target_length_bytes*2 << " characters";
     } else {
       return true;
     }
